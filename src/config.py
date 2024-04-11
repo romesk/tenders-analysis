09caa37a993aba32f5155e72a934b48ba6382365
Roman Skok <rma.skok@gmail.com>
2024-04-11 17:10:08 +0300

    feat: loading static files

--- src/config.py
+++ src/config.py
@@ -9,3 +9,15 @@ class CONFIG:
     class MONGO:
         URI = os.getenv("MONGO_URI")
         DB_NAME = os.getenv("MONGO_DB_NAME")
+
+        KVEDS_COLLECTION = "kveds"
+        KATOTTG_COLLECTION = "katottg"
+        DK_COLLECTION = "dk"
+
+    class FILES:
+        CWD = os.getcwd()
+        FILES_DIR = os.path.join(CWD, "files")
+
+        KVEDS_PATH = os.path.join(FILES_DIR, "kved_parsed.json")
+        KATOTTG_FILENAME = os.path.join(FILES_DIR, "katottg_parsed.json")
+        DK_FILENAME = os.path.join(FILES_DIR, "dk_parsed.json")
--- src/services/mongo.py
+++ src/services/mongo.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -13,6 +14,9 @@ class MongoService:
 
     def insert(self, collection: str, data):
         self.db[collection].insert_one(data)
+
+    def insert_many(self, collection: str, data: Iterable):
+        self.db[collection].insert_many(data)
 
     def find(self, collection: str, query):
         return self.db[collection].find(query)
@@ -31,3 +35,19 @@ class MongoService:
 
     def close(self):
         self.client.close()
+
+    def get_collections(self) -> list[str]:
+        return self.db.list_collection_names()
+
+    def create_collection_with_unique_index(
+        self, collection_name: str, unique_columns: list[str]
+    ) -> bool:
+        if collection_name in self.get_collections():
+            raise Exception(f"Collection {collection_name} already exists")
+
+        # create collection with unique index
+        self.db.create_collection(collection_name)
+        self.db[collection_name].create_index(
+            [(col, 1) for col in unique_columns], unique=True, name="unique_index"
+        )
+        return True
